d52ec8d7959975317889dce2d92e43d9bc905721
Dias Iskrayev <sparkymusic22@gmail.com>
2020-09-27 14:55:49 -0700

    Updates

---
+++ precache-manifest.c500a7eb6998887f33763e5aa2c422b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80417bfe7e2916c20ad74574ecc6f83",
+    "url": "/expense-app/index.html"
+  },
+  {
+    "revision": "07de7b5bb63856dd4b99",
+    "url": "/expense-app/static/css/main.36075247.chunk.css"
+  },
+  {
+    "revision": "e4f339179b4893169db0",
+    "url": "/expense-app/static/js/2.2477d6c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/expense-app/static/js/2.2477d6c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07de7b5bb63856dd4b99",
+    "url": "/expense-app/static/js/main.c86b5262.chunk.js"
+  },
+  {
+    "revision": "2c83ca02e6e1235c42ba",
+    "url": "/expense-app/static/js/runtime-main.fa6e95aa.js"
+  }
+]);
